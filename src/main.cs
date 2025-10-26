87b3494a0c1e80ecdc87ad564b24df98aff46549
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-10-25 21:18:42 -0400

    Error when command isn't recognized

--- src/main.cs
+++ src/main.cs
@@ -3,5 +3,9 @@ class Program
     static void Main()
     {
         Console.Write("$ ");
+
+        string command = Console.ReadLine() ?? "";
+
+        Console.Error.WriteLine($"{command}: command not found");
     }
 }
