e133d717d09a38c3497c680a1ea7aedafe12ad40
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-10-27 13:09:25 -0400

    Run external programs

--- src/CommandProcessor.cs
+++ src/CommandProcessor.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 class CommandProcessor
 {
     public void ProcessCommand(string command)
@@ -17,7 +19,41 @@ class CommandProcessor
         }
         else
         {
-            Console.WriteLine($"{args[0]}: command not found");
+            // Try to run a program
+
+            SystemCommandLookup commandLookup = new();
+            try
+            {
+                var program = commandLookup.GetCommandPath(args[0]);
+                var process = new Process
+                {
+                    StartInfo = new()
+                    {
+                        FileName = program,
+                        Arguments = string.Join(" ", args.Skip(1).ToArray()),
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+
+                process.Start();
+                process.OutputDataReceived += (s, e) => { if (e.Data != null) Console.WriteLine(e.Data); };
+                process.ErrorDataReceived += (s, e) => { if (e.Data != null) Console.Error.WriteLine(e.Data); };
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"{args[0]}: command not found");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
         }
     }
 
